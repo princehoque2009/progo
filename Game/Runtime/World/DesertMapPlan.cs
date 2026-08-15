7ae3ebaca9cd9604281f81289ff4b02599a248fb
princehoque2009 <historyancient475@gmail.com>
2026-08-15 05:50:19 -0300

    feat: define initial desert map layouts

---
+++ Game/Runtime/World/DesertMapPlan.cs
@@ -0,0 +1,28 @@
+namespace Progo.Game
+{
+    public static class DesertMapPlan
+    {
+        // Logical layout used to guide scene construction and procedural generation.
+        public static readonly string[] SunsetHighwayZones =
+        {
+            "SpawnTown",
+            "LongStraight",
+            "GasStationWest",
+            "CanyonTurn",
+            "DesertLookout",
+            "MotelRestStop",
+            "OldBridge",
+            "FinishOutpost"
+        };
+
+        public static readonly string[] RedCanyonZones =
+        {
+            "CanyonEntrance",
+            "Switchbacks",
+            "Tunnel",
+            "RiverBed",
+            "MiningCamp",
+            "CanyonOverlook"
+        };
+    }
+}
